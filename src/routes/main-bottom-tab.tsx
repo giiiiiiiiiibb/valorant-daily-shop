2b6707a73277be052a87dbbcaa3c6fd145004488
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-07-30 18:50:32 +0200

    rename tabs, remove reset logic, clean imports

--- src/routes/main-bottom-tab.tsx
+++ src/routes/main-bottom-tab.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 // components
 import SvgShop from "@/components/icon/shop";
 import SvgUser from "@/components/icon/user";
 import SvgUsers from "@/components/icon/users";
 // contexts
-import useAuthContext from "@/contexts/hook/use-auth-context";
 import useThemeContext from "@/contexts/hook/use-theme-context";
 // routes
 import Header from "@/routes/navigation/header";
 import StoreStackScreen from "@/routes/store-stack-screen";
 // screens
 import ProfileScreen from "@/screens/profile/profile-screen";
-// types
-import { EAuthContextType } from "@/types/context/auth";
+import Accounts from "@/screens/accounts";
 
 const BottomNavigation = createMaterialBottomTabNavigator();
 
@@ -22,14 +19,12 @@ const MainBottomTab = () => {
 
     const { colors } = useThemeContext();
 
-    const { dispatch } = useAuthContext();
-
     return (
         <>
             <Header />
             <BottomNavigation.Navigator
                 sceneAnimationEnabled
-                initialRouteName="shop"
+                initialRouteName="Shops"
                 inactiveColor="#7A7B7E"
                 activeColor={colors.primary}
                 sceneAnimationType="shifting"
@@ -37,14 +32,14 @@ const MainBottomTab = () => {
                 barStyle={{ justifyContent: "center", backgroundColor: "#1B1D21" }}
             >
                 <BottomNavigation.Screen
-                    name="shop"
+                    name="Shops"
                     component={StoreStackScreen}
                     options={{
                         tabBarIcon: ({ color }) => <SvgShop color={color} />,
                     }}
                 />
                 <BottomNavigation.Screen
-                    name="profile"
+                    name="Profile"
                     component={ProfileScreen}
                     options={{
                         tabBarIcon: ({ color }) => <SvgUser color={color} />,
@@ -52,20 +47,9 @@ const MainBottomTab = () => {
                 />
                 <BottomNavigation.Screen
                     name="Accounts"
-                    component={() => null}
+                    component={Accounts}
                     options={{
                         tabBarIcon: ({ color }) => <SvgUsers color={color} />,
-                    }}
-                    listeners={{
-                        tabPress: async () => {
-                            await AsyncStorage.removeItem("current_user");
-                            dispatch({
-                                type: EAuthContextType.INITIAL,
-                                payload: {
-                                    currentUser: null,
-                                },
-                            });
-                        },
                     }}
                 />
             </BottomNavigation.Navigator>
