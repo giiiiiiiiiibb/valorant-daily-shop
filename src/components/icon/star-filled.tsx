75d3fe019cfa6b7f20c01ea49a907ddde8e507b9
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-01 12:23:25 +0200

    add star-filled icon

---
+++ src/components/icon/star-filled.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const SvgStarFilled = ({ color = "#FFD700", size = 24 }: { color?: string; size?: number }) => (
+    <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+        <Path
+            d="M12 17.27L18.18 21L16.54 13.97L22 9.23999L14.81 8.62999L12 2L9.19 8.62999L2 9.23999L7.46 13.97L5.82 21L12 17.27Z"
+            fill={color}
+        />
+    </Svg>
+);
+
+export default SvgStarFilled;
