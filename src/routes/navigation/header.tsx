f5f885ceffc32e5b70e9a635f54ccc97ff04e8fa
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-06 11:10:15 +0200

    change styling

--- src/routes/navigation/header.tsx
+++ src/routes/navigation/header.tsx
@@ -11,75 +11,82 @@ import useAuthContext from "@/contexts/hook/use-auth-context";
 import useThemeContext from "@/contexts/hook/use-theme-context";
 
 type Props = {
-  activeTab: string;
+    activeTab: string;
 };
 
 const Header = ({ activeTab }: Props) => {
-  const { colors } = useThemeContext();
-  const { balance, gameName, tagLine } = useUserContext();
-  const { logoutAll } = useAuthContext();
+    const { colors } = useThemeContext();
+    const { balance, gameName, tagLine } = useUserContext();
+    const { logoutAll } = useAuthContext();
 
-  const renderRightComponent = () => {
-    switch (activeTab) {
-      case "Shops":
-        return (
-          <View style={styles.rightGroup}>
-            <CostPoint currencyId="vp" cost={balance.valorantPoint ?? 0} textVariant="bodyMedium" />
-            <CostPoint currencyId="rp" cost={balance.radianitePoint ?? 0} textVariant="bodyMedium" />
-            <CostPoint currencyId="kc" cost={balance.kingdomCredit ?? 0} textVariant="bodyMedium" />
-          </View>
-        );
-      case "Profile":
-        return (
-          <Text variant="titleSmall" style={styles.userInfo}>
-            {gameName}#{tagLine}
-          </Text>
-        );
-      case "Accounts":
-        return (
-          <Button
-            onPress={logoutAll}
-            icon={<SvgLogout color={colors.primary} />}
-            variant="icon"
-            style={{ padding: 8 }}
-            underlayColor="#222429"
-          />
-        );
-      default:
-        return null;
-    }
-  };
+    const renderRightComponent = () => {
+        switch (activeTab) {
+            case "Shops":
+                return (
+                    <View style={styles.rightGroup}>
+                        <CostPoint currencyId="vp" cost={balance.valorantPoint ?? 0} textVariant="bodyMedium" />
+                        <CostPoint currencyId="rp" cost={balance.radianitePoint ?? 0} textVariant="bodyMedium" />
+                        <CostPoint currencyId="kc" cost={balance.kingdomCredit ?? 0} textVariant="bodyMedium" />
+                    </View>
+                );
+            case "Profile":
+                return (
+                    <Text variant="titleSmall" style={styles.userInfo}>
+                        {gameName}#{tagLine}
+                    </Text>
+                );
+            case "Accounts":
+                return (
+                    <Button
+                        onPress={logoutAll}
+                        icon={<SvgLogout color={colors.primary} />}
+                        variant="icon"
+                        style={styles.logoutButton}
+                        underlayColor="#222429"
+                    />
+                );
+            default:
+                return null;
+        }
+    };
 
-  return (
-    <View style={[styles.header, { backgroundColor: colors.background }]}>
-      <Text variant="displayMedium" style={styles.title}>
-        {activeTab.toUpperCase()}
-      </Text>
-      {renderRightComponent()}
-    </View>
-  );
+    return (
+        <View style={[styles.header, { backgroundColor: colors.background }]}>
+            <Text variant="displayMedium" style={styles.title}>
+                {activeTab.toUpperCase()}
+            </Text>
+            <View style={styles.rightComponentWrapper}>{renderRightComponent()}</View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  header: {
-    height: 64,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 16,
-  },
-  title: {
-    fontFamily: "Vandchrome",
-    textTransform: "uppercase",
-  },
-  rightGroup: {
-    gap: 16,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  userInfo: {
-    opacity: 0.5,
-  },
+    header: {
+        height: 64,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        justifyContent: "space-between",
+    },
+    title: {
+        fontFamily: "Vandchrome",
+        textTransform: "uppercase",
+    },
+    rightComponentWrapper: {
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    rightGroup: {
+        gap: 16,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    userInfo: {
+        opacity: 0.5,
+    },
+    logoutButton: {
+        padding: 8,
+    },
 });
 
 export default Header;
