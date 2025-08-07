5654a2006d945f01664fa912297b7ddbd88b42c3
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-07 07:41:47 +0200

    refactoring done

--- src/api/valorant-provider.tsx
+++ src/api/valorant-provider.tsx
@@ -141,7 +141,7 @@ const valorantProvider = {
         const options = {
             method: "POST",
             url: `https://pd.${pp}.a.pvp.net/store/v3/storefront/${sub}`,
-            data: {},
+            data: "{}",
             headers: {
                 Authorization: `Bearer ${accessToken}`,
                 "X-Riot-Entitlements-JWT": `${entitlementsToken}`,
