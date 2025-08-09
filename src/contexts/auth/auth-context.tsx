cbe6bcaceca8024fc842cbf22c174a1e7450e631
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 13:32:33 +0200

    update auth-context.tsx

--- src/contexts/auth/auth-context.tsx
+++ src/contexts/auth/auth-context.tsx
@@ -1,19 +1,17 @@
 import { createContext } from "react";
 // types
-import { IAuthContext } from "@/types/context/auth";
-
-// ----------------------------------------
+import type { IAuthContext } from "@/types/context/auth";
 
 export const initialAuthState: IAuthContext = {
-    // fn
-    login: () => Promise.resolve(),
-    logoutUser: () => Promise.resolve(),
-    dispatch: () => {},
-    // states
-    isLoading: false,
-    isSignout: false,
-    isInitialized: false,
-    currentUser: null,
+  // fns
+  initialize: async () => {},
+  selectAccount: async () => ({ needsInteractive: true }),
+  loginInteractive: async () => {},
+  logoutUser: async () => {},
+  logoutAll: async () => {},
+  // state
+  state: "initializing",
+  currentUser: null,
 };
 
 export const AuthContext = createContext<IAuthContext>(initialAuthState);
