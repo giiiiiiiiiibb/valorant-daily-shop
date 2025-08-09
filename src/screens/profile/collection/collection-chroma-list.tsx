0723fe4947d0cd56c6c8e07a6b12e460f2bde87e
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 10:35:01 +0200

    update collection-chroma-list.tsx

--- src/screens/profile/collection/collection-chroma-list.tsx
+++ src/screens/profile/collection/collection-chroma-list.tsx
@@ -9,92 +9,90 @@ import useThemeContext from "@/contexts/hook/use-theme-context";
 import useProfileContext from "@/contexts/hook/use-profile-context";
 // types
 import { WeaponChroma, WeaponSkin } from "@/types/api/shop/weapon-skin";
+// utils
+import { hexToRgba } from "@/utils/color";
 
 type Props = {
-    currentSkin: WeaponSkin;
-    currentChromaIndex: number;
-    handleChromaPress: (index: number, fullRender: string) => void;
+  currentSkin: WeaponSkin;
+  currentChromaIndex: number;
+  handleChromaPress: (index: number, fullRender: string) => void;
 };
 
 const CollectionChromaList: React.FC<Props> = ({ currentChromaIndex, currentSkin, handleChromaPress }) => {
+  const { palette } = useThemeContext();
+  const { skinVariants, favoriteSkins } = useProfileContext();
 
-    const { colors } = useThemeContext();
-
-    const { skinVariants, favoriteSkins } = useProfileContext();
-
-    const renderChroma = (chroma: WeaponChroma, index: number) => {
-        const isCurrentChroma = currentChromaIndex === index;
-        const isLocked = !skinVariants?.Entitlements.some(variant => variant.ItemID === chroma.uuid);
-        const isFavorite = Object.keys(favoriteSkins?.FavoritedContent || {}).includes(chroma.uuid);
-
-        return (
-            <TouchableRipple
-                borderless
-                key={index}
-                onPress={() => handleChromaPress(index, chroma.fullRender)}
-                style={[
-                    styles.chromaItem,
-                    {
-                        borderColor: isCurrentChroma ? colors.primary : "#222429",
-                    },
-                ]}
-            >
-                <>
-                    <Image source={{ uri: chroma.swatch }} style={styles.chromaImage} />
-                    {(isLocked && index !== 0) && (
-                        <View style={styles.lockOverlay}>
-                            <SvgLock color="white" width={32} height={32} />
-                        </View>
-                    )}
-                    {isFavorite && (
-                        <View style={styles.checkOverlay}>
-                            <SvgFavorite color="#FFE500" width={32} height={32} />
-                        </View>
-                    )}
-                </>
-            </TouchableRipple>
-        );
-    };
+  const renderChroma = (chroma: WeaponChroma, index: number) => {
+    const isCurrentChroma = currentChromaIndex === index;
+    const isLocked = !skinVariants?.Entitlements.some((variant) => variant.ItemID === chroma.uuid);
+    const isFavorite = Object.keys(favoriteSkins?.FavoritedContent || {}).includes(chroma.uuid);
 
     return (
-        <View style={styles.chromaContainer}>
-            {currentSkin.chromas.map(renderChroma)}
-        </View>
+      <TouchableRipple
+        borderless
+        key={index}
+        onPress={() => handleChromaPress(index, chroma.fullRender)}
+        style={[
+          styles.chromaItem,
+          {
+            backgroundColor: palette.card,
+            borderColor: isCurrentChroma ? palette.primary : palette.card,
+          },
+        ]}
+        rippleColor={hexToRgba(palette.primary, 0.2)}
+      >
+        <>
+          <Image source={{ uri: chroma.swatch }} style={styles.chromaImage} />
+          {isLocked && index !== 0 && (
+            <View
+              style={[
+                styles.overlayBase,
+                { backgroundColor: hexToRgba(palette.text, 0.2), alignItems: "center", justifyContent: "center" },
+              ]}
+            >
+              <SvgLock color={palette.text} width={32} height={32} />
+            </View>
+          )}
+          {isFavorite && (
+            <View
+              style={[
+                styles.overlayBase,
+                { backgroundColor: hexToRgba(palette.text, 0.5), alignItems: "center", justifyContent: "center" },
+              ]}
+            >
+              <SvgFavorite color="#FFE500" width={32} height={32} />
+            </View>
+          )}
+        </>
+      </TouchableRipple>
     );
+  };
+
+  return <View style={styles.chromaContainer}>{currentSkin.chromas.map(renderChroma)}</View>;
 };
 
 const styles = StyleSheet.create({
-    chromaContainer: {
-        gap: 16,
-        flexDirection: "row",
-    },
-    chromaItem: {
-        width: 64,
-        height: 64,
-        padding: 4,
-        borderWidth: 2,
-        borderRadius: 22,
-        position: "relative",
-        justifyContent: "center",
-        backgroundColor: "#222429",
-    },
-    chromaImage: {
-        width: 52,
-        height: 52,
-        borderRadius: 16,
-    },
-    lockOverlay: {
-        ...StyleSheet.absoluteFillObject,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "rgba(255, 70, 86, .2)",
-    },
-    checkOverlay: {
-        ...StyleSheet.absoluteFillObject,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "rgba(34, 36, 41, .5)",
-    },
+  chromaContainer: {
+    gap: 16,
+    flexDirection: "row",
+  },
+  chromaItem: {
+    width: 64,
+    height: 64,
+    padding: 4,
+    borderWidth: 2,
+    borderRadius: 22,
+    position: "relative",
+    justifyContent: "center",
+  },
+  chromaImage: {
+    width: 52,
+    height: 52,
+    borderRadius: 16,
+  },
+  overlayBase: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 export default React.memo(CollectionChromaList);
