6b84edc185e0abaaadee5ca30c11f1ae5767bf16
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 10:32:20 +0200

    update error.tsx

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -5,24 +5,23 @@ import Text from "@/components/typography/text";
 import useThemeContext from "@/contexts/hook/use-theme-context";
 
 const Error = () => {
+  const { palette } = useThemeContext();
 
-    const { colors } = useThemeContext();
-
-    return (
-        <View
-            style={{
-                backgroundColor: colors.card,
-                borderRadius: 16,
-                padding: 16,
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 16,
-            }}
-        >
-            <Image source={require("../../../assets/error.png")} style={{ width: 50, height: 50 }} />
-            <Text variant="titleMedium">Error when trying to get resource</Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        backgroundColor: palette.card,
+        borderRadius: 16,
+        padding: 16,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 16,
+      }}
+    >
+      <Image source={require("../../../assets/error.png")} style={{ width: 50, height: 50 }} />
+      <Text variant="titleMedium">Error when trying to get resource</Text>
+    </View>
+  );
 };
 
 export default Error;
