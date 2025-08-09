eb6c5b1695cca01028b4b448ad054dae464be3fb
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 10:46:51 +0200

    update collection-card.tsx

--- src/screens/profile/collection/collection-card.tsx
+++ src/screens/profile/collection/collection-card.tsx
@@ -16,100 +16,98 @@ import { PlayerLoadoutGun } from "@/types/api/player-loadout";
 // utils
 import { getWeaponName } from "@/utils/format-string";
 import { getWeaponByUuid } from "@/utils/weapon-name";
-//
+import { hexToRgba } from "@/utils/color";
+
 import CollectionCardSkeleton from "./collection-card-skeleton";
 
 type Props = {
-    playerLoadoutGun: PlayerLoadoutGun;
-}
+  playerLoadoutGun: PlayerLoadoutGun;
+};
 
 const CollectionCard = ({ playerLoadoutGun }: Props) => {
+  const { palette } = useThemeContext();
+  const { setCurrentPlayerLoadoutSkin, setCurrentPlayerLoadoutGun } = useProfileContext();
+  const navigate = useNavigation<NavigationProp>();
 
-    const { colors } = useThemeContext();
+  const {
+    data: weaponSkinData,
+    error: weaponSkinError,
+    isLoading: isLoadingWeapon,
+  } = useGetWeaponByLevelIdQuery(playerLoadoutGun.SkinLevelID);
 
-    const { setCurrentPlayerLoadoutSkin , setCurrentPlayerLoadoutGun} = useProfileContext();
+  const {
+    data: themeData,
+    error: themeError,
+    isLoading: isLoadingTheme,
+  } = useGetThemeByIdQuery(weaponSkinData?.data?.themeUuid || "");
 
-    const navigate = useNavigation<NavigationProp>();
+  const skinData = weaponSkinData?.data;
 
-    const {
-        data: weaponSkinData,
-        error: weaponSkinError,
-        isLoading: isLoadingWeapon,
-    } = useGetWeaponByLevelIdQuery(playerLoadoutGun.SkinLevelID);
+  const filteredDisplayName = useMemo(() => {
+    if (!weaponSkinData?.data?.displayName || !themeData?.data?.displayName) return "";
+    return getWeaponName(weaponSkinData.data.displayName, themeData.data.displayName);
+  }, [weaponSkinData?.data?.displayName, themeData?.data?.displayName]);
 
-    const {
-        data: themeData,
-        error: themeError,
-        isLoading: isLoadingTheme,
-    } = useGetThemeByIdQuery(weaponSkinData?.data?.themeUuid || "");
+  const onCardPress = useCallback(() => {
+    if (!weaponSkinData || !themeData) return;
+    setCurrentPlayerLoadoutSkin(weaponSkinData.data);
+    setCurrentPlayerLoadoutGun(playerLoadoutGun);
+    navigate.navigate("CollectionDetails");
+  }, [navigate, weaponSkinData, themeData, playerLoadoutGun, setCurrentPlayerLoadoutGun, setCurrentPlayerLoadoutSkin]);
 
-    const skinData = weaponSkinData?.data;
+  if (isLoadingWeapon || isLoadingTheme) {
+    return <CollectionCardSkeleton />;
+  }
 
-    const filteredDisplayName = useMemo(() => {
-        if (!weaponSkinData?.data?.displayName || !themeData?.data?.displayName) return "";
-        return getWeaponName(weaponSkinData.data.displayName, themeData.data.displayName);
-    }, [weaponSkinData?.data?.displayName, themeData?.data?.displayName]);
+  if (weaponSkinError || !skinData || themeError || !themeData) {
+    return <Error />;
+  }
 
-    const onCardPress = useCallback(() => {
-        if (!weaponSkinData || !themeData) return;
-        setCurrentPlayerLoadoutSkin(weaponSkinData.data);
-        setCurrentPlayerLoadoutGun(playerLoadoutGun);
-        navigate.navigate("CollectionDetails");
-    }, [navigate, weaponSkinData, themeData, filteredDisplayName, playerLoadoutGun]);
-
-    if (isLoadingWeapon || isLoadingTheme) {
-        return <CollectionCardSkeleton />;
-    }
-
-    if (weaponSkinError || !skinData || themeError || !themeData) {
-        return <Error />;
-    }
-
-    return (
-        <TouchableRipple
-            borderless
-            onPress={onCardPress}
-            key={playerLoadoutGun.ID}
-            rippleColor="rgba(255, 70, 86, .20)"
-            style={[styles.container, { backgroundColor: colors.card }]}
-        >
-            <ImageBackground
-                resizeMode="cover"
-                style={styles.imageBackground}
-                source={{ uri: skinData.wallpaper }}
-            >
-                <Text variant="titleLarge" numberOfLines={1}>{getWeaponByUuid(playerLoadoutGun.ID)}</Text>
-                <Image
-                    resizeMode="contain"
-                    style={styles.skinImage}
-                    source={{ uri: skinData.chromas.find((weaponChroma) => weaponChroma.uuid === playerLoadoutGun.ChromaID)?.fullRender }}
-                />
-            </ImageBackground>
-        </TouchableRipple>
-    );
+  return (
+    <TouchableRipple
+      borderless
+      onPress={onCardPress}
+      key={playerLoadoutGun.ID}
+      rippleColor={hexToRgba(palette.primary, 0.2)}
+      style={[styles.container, { backgroundColor: palette.card }]}
+    >
+      <ImageBackground resizeMode="cover" style={styles.imageBackground} source={{ uri: skinData.wallpaper }}>
+        <Text variant="titleLarge" numberOfLines={1}>
+          {getWeaponByUuid(playerLoadoutGun.ID)}
+        </Text>
+        <Image
+          resizeMode="contain"
+          style={styles.skinImage}
+          source={{
+            uri: skinData.chromas.find((weaponChroma) => weaponChroma.uuid === playerLoadoutGun.ChromaID)?.fullRender,
+          }}
+        />
+      </ImageBackground>
+    </TouchableRipple>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        borderRadius: 16,
-    },
-    imageBackground: {
-        gap: 16,
-        padding: 16,
-    },
-    skinImage: {
-        flex: 1,
-        height: 70,
-    },
-    footer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    contentTierIcon: {
-        width: 32,
-        height: 32,
-    },
+  container: {
+    flex: 1,
+    borderRadius: 16,
+  },
+  imageBackground: {
+    gap: 16,
+    padding: 16,
+  },
+  skinImage: {
+    flex: 1,
+    height: 70,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  contentTierIcon: {
+    width: 32,
+    height: 32,
+  },
 });
 
 export default React.memo(CollectionCard);
