25522ec9e608f0e2b64b4ace404c7e1dc7d522d0
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 09:40:37 +0200

    update plugin.tsx

--- src/screens/plugin/plugin.tsx
+++ src/screens/plugin/plugin.tsx
@@ -13,67 +13,47 @@ import CardFactory from "@/factories/card-factory";
 import { PluginDetailScreenProps } from "@/types/router/navigation";
 
 const Plugin = ({ route, navigation }: PluginDetailScreenProps) => {
-    const { colors } = useThemeContext();
+  const { palette } = useThemeContext();
+  const { plugin } = route.params;
 
-    const { plugin } = route.params;
+  const { data, isLoading, error } = useGetBundleByIdQuery(plugin.PurchaseInformation.DataAssetID);
 
-    const { data, isLoading, error } = useGetBundleByIdQuery(plugin.PurchaseInformation.DataAssetID);
+  const goBack = useCallback(() => navigation.goBack(), [navigation]);
 
-    const goBack = useCallback(() => navigation.goBack(), [navigation]);
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error || !data) return <Text>Error...</Text>;
 
-    if (isLoading) return <Text>Loading...</Text>;
-    if (error || !data) return <Text>Error...</Text>;
+  const offerData = data.data;
 
-    const offerData = data.data;
-
-    return (
-        <View style={[styles.container, { backgroundColor: colors.background }]}>
-            <View style={styles.header}>
-                <IconButton icon="arrow-left" iconColor="#fff" size={32} onPress={goBack} />
-                <View style={styles.titleContainer}>
-                    <Text variant="titleLarge">{offerData.displayName}</Text>
-                </View>
-                <View style={styles.placeholder} />
-            </View>
-            <FlatList
-                overScrollMode="never"
-                data={plugin.SubOffers}
-                snapToAlignment="center"
-                style={styles.list}
-                contentContainerStyle={styles.listContent}
-                showsVerticalScrollIndicator={false}
-                renderItem={({ item, index }) => (
-                    <CardFactory offer={item.PurchaseInformation} key={index} />
-                )}
-            />
+  return (
+    <View style={[styles.container, { backgroundColor: palette.background }]}>
+      <View style={styles.header}>
+        <IconButton icon="arrow-left" iconColor={palette.text} size={32} onPress={goBack} />
+        <View style={styles.titleContainer}>
+          <Text variant="titleLarge">{offerData.displayName}</Text>
         </View>
-    );
+        <View style={styles.placeholder} />
+      </View>
+      <FlatList
+        overScrollMode="never"
+        data={plugin.SubOffers}
+        snapToAlignment="center"
+        style={styles.list}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item, index }) => <CardFactory offer={item.PurchaseInformation} key={index} />}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    },
-    header: {
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    titleContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    placeholder: {
-        width: 72,
-        height: 72,
-    },
-    list: {
-        marginTop: 16,
-    },
-    listContent: {
-        gap: 16,
-    },
+  container: { flex: 1, padding: 16 },
+  header: { flexDirection: "row", alignItems: "center" },
+  titleContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
+  placeholder: { width: 72, height: 72 },
+  list: { marginTop: 16 },
+  listContent: { gap: 16 },
 });
 
 export default React.memo(Plugin);
