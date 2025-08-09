0adad0b4f019f3d63dc7ae5eb0249010e4febed3
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 09:52:31 +0200

    update loading.tsx

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -4,14 +4,13 @@ import { ActivityIndicator, View } from "react-native";
 import useThemeContext from "@/contexts/hook/use-theme-context";
 
 const Loading = (): ReactElement => {
+  const { palette } = useThemeContext();
 
-    const { colors } = useThemeContext();
-
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: colors.background }}>
-            <ActivityIndicator color={colors.text} size="large" />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: palette.background }}>
+      <ActivityIndicator color={palette.text} size="large" />
+    </View>
+  );
 };
 
 export default Loading;
