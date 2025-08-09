9d4abe561f03341a8a0ba9c9adb2d1264c973eb4
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 09:49:47 +0200

    update tab-header.tsx

--- src/routes/navigation/tab-header.tsx
+++ src/routes/navigation/tab-header.tsx
@@ -9,84 +9,86 @@ import Button from "@/components/button/button";
 import useUserContext from "@/contexts/hook/use-user-context";
 import useAuthContext from "@/contexts/hook/use-auth-context";
 import useThemeContext from "@/contexts/hook/use-theme-context";
+// utils
+import { hexToRgba } from "@/utils/color";
 
 type Props = {
-    activeTab: string;
+  activeTab: string;
 };
 
 const TabHeader = ({ activeTab }: Props) => {
-    const { colors } = useThemeContext();
-    const { balance, gameName, tagLine } = useUserContext();
-    const { logoutAll } = useAuthContext();
+  const { palette } = useThemeContext();
+  const { balance, gameName, tagLine } = useUserContext();
+  const { logoutAll } = useAuthContext();
 
-    const renderRightComponent = () => {
-        switch (activeTab) {
-            case "Shops":
-                return (
-                    <View style={styles.rightGroup}>
-                        <CostPoint currencyId="vp" cost={balance.valorantPoint ?? 0} textVariant="bodyMedium" />
-                        <CostPoint currencyId="rp" cost={balance.radianitePoint ?? 0} textVariant="bodyMedium" />
-                        <CostPoint currencyId="kc" cost={balance.kingdomCredit ?? 0} textVariant="bodyMedium" />
-                    </View>
-                );
-            case "Profile":
-                return (
-                    <Text variant="titleSmall" style={styles.userInfo}>
-                        {gameName}#{tagLine}
-                    </Text>
-                );
-            case "Accounts":
-                return (
-                    <Button
-                        onPress={logoutAll}
-                        icon={<SvgLogout color={colors.primary} />}
-                        variant="icon"
-                        style={styles.logoutButton}
-                        underlayColor="#222429"
-                    />
-                );
-            default:
-                return null;
-        }
-    };
+  const renderRightComponent = () => {
+    switch (activeTab) {
+      case "Shops":
+        return (
+          <View style={styles.rightGroup}>
+            <CostPoint currencyId="vp" cost={balance.valorantPoint ?? 0} textVariant="bodyMedium" />
+            <CostPoint currencyId="rp" cost={balance.radianitePoint ?? 0} textVariant="bodyMedium" />
+            <CostPoint currencyId="kc" cost={balance.kingdomCredit ?? 0} textVariant="bodyMedium" />
+          </View>
+        );
+      case "Profile":
+        return (
+          <Text variant="titleSmall" style={styles.userInfo}>
+            {gameName}#{tagLine}
+          </Text>
+        );
+      case "Accounts":
+        return (
+          <Button
+            onPress={logoutAll}
+            icon={<SvgLogout color={palette.primary} />}
+            variant="icon"
+            style={styles.logoutButton}
+            underlayColor={hexToRgba(palette.text, 0.08)}
+          />
+        );
+      default:
+        return null;
+    }
+  };
 
-    return (
-        <View style={[styles.header, { backgroundColor: colors.background }]}>
-            <Text variant="displayMedium" style={styles.title}>
-                {typeof activeTab === "string" ? activeTab.toUpperCase() : ""}
-            </Text>
-            <View style={styles.rightComponentWrapper}>{renderRightComponent()}</View>
-        </View>
-    );
+  return (
+    <View style={[styles.header, { backgroundColor: palette.background }]}>
+      <Text variant="displayMedium" style={styles.title}>
+        {typeof activeTab === "string" ? activeTab.toUpperCase() : ""}
+      </Text>
+      <View style={styles.rightComponentWrapper}>{renderRightComponent()}</View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        height: 64,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingHorizontal: 16,
-        justifyContent: "space-between",
-    },
-    title: {
-        fontFamily: "Vandchrome",
-        textTransform: "uppercase",
-    },
-    rightComponentWrapper: {
-        justifyContent: "flex-end",
-        alignItems: "center",
-    },
-    rightGroup: {
-        gap: 16,
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    userInfo: {
-        opacity: 0.5,
-    },
-    logoutButton: {
-        padding: 8,
-    },
+  header: {
+    height: 64,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    justifyContent: "space-between",
+  },
+  title: {
+    fontFamily: "Vandchrome",
+    textTransform: "uppercase",
+  },
+  rightComponentWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  rightGroup: {
+    gap: 16,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  userInfo: {
+    opacity: 0.5,
+  },
+  logoutButton: {
+    padding: 8,
+  },
 });
 
 export default TabHeader;
