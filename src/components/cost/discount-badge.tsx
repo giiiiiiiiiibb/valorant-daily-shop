6c0755678ebea5daddb85e13d65e99ce7990f6ab
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 10:33:52 +0200

    update discount-badge.tsx

--- src/components/cost/discount-badge.tsx
+++ src/components/cost/discount-badge.tsx
@@ -4,47 +4,48 @@ import { ReactElement } from "react";
 import Text from "@/components/typography/text";
 // contexts
 import useThemeContext from "@/contexts/hook/use-theme-context";
+// utils
+import { hexToRgba } from "@/utils/color";
 
 type Props = {
-    discount: number;
-}
+  discount: number;
+};
 
 const DiscountBadge = ({ discount }: Props): ReactElement => {
+  const { palette } = useThemeContext();
 
-    const { colors } = useThemeContext();
-
-    return (
-        <View
-            style={{
-                position: "absolute",
-                right: 0,
-                width: 50,
-                height: 50,
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "transparent",
-            }}
-        >
-            <View
-                style={{
-                    position: "absolute",
-                    right: -1,
-                    width: 0,
-                    height: 0,
-                    borderStyle: "solid",
-                    borderTopWidth: 100,
-                    borderLeftWidth: 100,
-                    borderRightWidth: 0,
-                    borderBottomWidth: 0,
-                    borderLeftColor: "transparent",
-                    borderTopColor: "rgba(0, 0, 0, .5)",
-                }}
-            />
-            <Text variant="titleMedium" style={{ color: colors.primary, opacity: 1 }} numberOfLines={1}>
-                -{discount}%
-            </Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        position: "absolute",
+        right: 0,
+        width: 50,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "transparent",
+      }}
+    >
+      <View
+        style={{
+          position: "absolute",
+          right: -1,
+          width: 0,
+          height: 0,
+          borderStyle: "solid",
+          borderTopWidth: 100,
+          borderLeftWidth: 100,
+          borderRightWidth: 0,
+          borderBottomWidth: 0,
+          borderLeftColor: "transparent",
+          borderTopColor: hexToRgba(palette.text, 0.4),
+        }}
+      />
+      <Text variant="titleMedium" style={{ color: palette.primary, opacity: 1 }} numberOfLines={1}>
+        -{discount}%
+      </Text>
+    </View>
+  );
 };
 
 export default DiscountBadge;
