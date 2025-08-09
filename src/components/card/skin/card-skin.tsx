13691a0a7bededdc0eadcc97759e2a8c0e28724d
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 09:30:21 +0200

    update card-skin.tsx

--- src/components/card/skin/card-skin.tsx
+++ src/components/card/skin/card-skin.tsx
@@ -5,6 +5,7 @@ import { Image, ImageBackground, View, StyleSheet } from "react-native";
 // api
 import { useGetThemeByIdQuery, useGetWeaponByLevelIdQuery } from "@/api/rtk-valorant-api";
 // components
+import Loading from "@/components/loading/loading";
 import Error from "@/components/error/error";
 import Text from "@/components/typography/text";
 import CostPoint from "@/components/cost/cost-point";
@@ -17,105 +18,107 @@ import { NavigationProp } from "@/types/router/navigation";
 // utils
 import { getWeaponName } from "@/utils/format-string";
 import { getContentTierIcon } from "@/utils/content-tier-icon";
-import Loading from "@/components/loading/loading";
+import { hexToRgba } from "@/utils/color";
 
 type Props = {
-    offer: Offer;
-}
+  offer: Offer;
+};
 
 const CardSkin = ({ offer }: Props) => {
-    const { colors } = useThemeContext();
-    const navigate = useNavigation<NavigationProp>();
+  const { palette } = useThemeContext();
+  const navigate = useNavigation<NavigationProp>();
 
-    const {
-        data: weaponSkinData,
-        error: weaponSkinError,
-        isLoading: isLoadingWeapon,
-    } = useGetWeaponByLevelIdQuery(offer.Rewards[0].ItemID);
+  const {
+    data: weaponSkinData,
+    error: weaponSkinError,
+    isLoading: isLoadingWeapon,
+  } = useGetWeaponByLevelIdQuery(offer.Rewards[0].ItemID);
 
-    const {
-        data: themeData,
-        error: themeError,
-        isLoading: isLoadingTheme,
-    } = useGetThemeByIdQuery(weaponSkinData?.data?.themeUuid || "");
+  const {
+    data: themeData,
+    error: themeError,
+    isLoading: isLoadingTheme,
+  } = useGetThemeByIdQuery(weaponSkinData?.data?.themeUuid || "");
 
-    const skinData = weaponSkinData?.data;
+  const skinData = weaponSkinData?.data;
 
-    const filteredDisplayName = useMemo(() => {
-        if (!weaponSkinData?.data?.displayName || !themeData?.data?.displayName) return "";
-        return getWeaponName(weaponSkinData.data.displayName, themeData.data.displayName);
-    }, [weaponSkinData?.data?.displayName, themeData?.data?.displayName]);
+  const filteredDisplayName = useMemo(() => {
+    if (!weaponSkinData?.data?.displayName || !themeData?.data?.displayName) return "";
+    return getWeaponName(weaponSkinData.data.displayName, themeData.data.displayName);
+  }, [weaponSkinData?.data?.displayName, themeData?.data?.displayName]);
 
-    const onCardPress = useCallback(() => {
-        if (!weaponSkinData || !themeData) return;
-        navigate.navigate("SkinDetails", {
-            skin: weaponSkinData.data,
-            skinType: filteredDisplayName,
-            theme: themeData.data,
-        });
-    }, [navigate, weaponSkinData, themeData, filteredDisplayName]);
+  const onCardPress = useCallback(() => {
+    if (!weaponSkinData || !themeData) return;
+    navigate.navigate("SkinDetails", {
+      skin: weaponSkinData.data,
+      skinType: filteredDisplayName,
+      theme: themeData.data,
+    });
+  }, [navigate, weaponSkinData, themeData, filteredDisplayName]);
 
-    if (isLoadingWeapon || isLoadingTheme) {
-        return <Loading />;
-    }
+  if (isLoadingWeapon || isLoadingTheme) {
+    return <Loading />;
+  }
 
-    if (weaponSkinError || !skinData || themeError || !themeData) {
-        return <Error />;
-    }
+  if (weaponSkinError || !skinData || themeError || !themeData) {
+    return <Error />;
+  }
 
-    return (
-        <TouchableRipple
-            borderless
-            key={offer.OfferID}
-            onPress={onCardPress}
-            rippleColor="rgba(255, 70, 86, .20)"
-            style={[styles.container, { backgroundColor: colors.card }]}
-        >
-            <ImageBackground
-                resizeMode="cover"
-                source={{ uri: skinData.wallpaper }}
-                style={styles.imageBackground}
-            >
-                <Text variant="titleLarge" numberOfLines={1}>{filteredDisplayName}</Text>
-                <Image
-                    resizeMode="contain"
-                    style={styles.skinImage}
-                    source={{ uri: skinData.displayIcon ?? skinData.chromas[0].displayIcon }}
-                />
-                <View style={styles.footer}>
-                    <CostPoint currencyId={Object.keys(offer.Cost)[0]} cost={offer.Cost[Object.keys(offer.Cost)[0]]} />
-                    <Image
-                        resizeMode="cover"
-                        style={styles.contentTierIcon}
-                        source={getContentTierIcon(skinData.contentTierUuid)}
-                    />
-                </View>
-            </ImageBackground>
-        </TouchableRipple>
-    );
+  return (
+    <TouchableRipple
+      borderless
+      key={offer.OfferID}
+      onPress={onCardPress}
+      rippleColor={hexToRgba(palette.primary, 0.2)}
+      style={[styles.container, { backgroundColor: palette.card }]}
+    >
+      <ImageBackground
+        resizeMode="cover"
+        source={{ uri: skinData.wallpaper }}
+        style={styles.imageBackground}
+      >
+        <Text variant="titleLarge" numberOfLines={1}>
+          {filteredDisplayName}
+        </Text>
+        <Image
+          resizeMode="contain"
+          style={styles.skinImage}
+          source={{ uri: skinData.displayIcon ?? skinData.chromas[0].displayIcon }}
+        />
+        <View style={styles.footer}>
+          <CostPoint currencyId={Object.keys(offer.Cost)[0]} cost={offer.Cost[Object.keys(offer.Cost)[0]]} />
+          <Image
+            resizeMode="cover"
+            style={styles.contentTierIcon}
+            source={getContentTierIcon(skinData.contentTierUuid)}
+          />
+        </View>
+      </ImageBackground>
+    </TouchableRipple>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        borderRadius: 16,
-    },
-    imageBackground: {
-        gap: 16,
-        padding: 16,
-    },
-    skinImage: {
-        flex: 1,
-        height: 70,
-    },
-    footer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    contentTierIcon: {
-        width: 32,
-        height: 32,
-    },
+  container: {
+    flex: 1,
+    borderRadius: 16,
+  },
+  imageBackground: {
+    gap: 16,
+    padding: 16,
+  },
+  skinImage: {
+    flex: 1,
+    height: 70,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  contentTierIcon: {
+    width: 32,
+    height: 32,
+  },
 });
 
 export default React.memo(CardSkin);
