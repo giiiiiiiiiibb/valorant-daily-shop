50cd90e5180e48f1c59e863ba2ce5b614c3ab5ab
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 13:28:22 +0200

    update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,17 +1,25 @@
 import axios from "axios";
 import { clearSecureStore } from "./secure-store";
 
-// ----------------------------------------------------------------------
-
+/**
+ * Axios instance with a conservative 401 handler.
+ * Avoid logging raw error objects to prevent leaking sensitive data.
+ */
 const axiosInstance = axios.create();
 
 axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        if (error.response && error.response.status === 401) {
-            await clearSecureStore();
-        }
-        await Promise.reject((error.response && error.response.data) || "Something went wrong");
-    },
+  (response) => response,
+  async (error) => {
+    try {
+      if (error?.response?.status === 401) {
+        await clearSecureStore();
+      }
+    } catch {
+      // ignore
+    }
+    // Return a generic error to the app layer
+    return Promise.reject("Request failed");
+  }
 );
+
 export default axiosInstance;
