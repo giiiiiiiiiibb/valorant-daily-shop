61fae9c862fd13cc104465ec83ff131f56160f14
giiiiiiiiiibb <114148716+giiiiiiiiiibb@users.noreply.github.com>
2025-08-09 10:13:43 +0200

    update card-spray.tsx

--- src/components/card/spray/card-spray.tsx
+++ src/components/card/spray/card-spray.tsx
@@ -16,107 +16,96 @@ import { Offer } from "@/types/api/shop";
 import { NavigationProp } from "@/types/router/navigation";
 // utils
 import { removeCardType } from "@/utils/format-string";
+import { hexToRgba } from "@/utils/color";
 
 type CardSprayProps = {
-    offer: Offer;
+  offer: Offer;
 };
 
 const CardSpray = ({ offer }: CardSprayProps) => {
-    const navigate = useNavigation<NavigationProp>();
-    const { colors } = useThemeContext();
+  const navigate = useNavigation<NavigationProp>();
+  const { palette } = useThemeContext();
 
-    const {
-        data: sprayData,
-        error: sprayError,
-        isLoading: isLoadingSpray,
-    } = useGetSprayByIdQuery(offer.Rewards[0].ItemID);
+  const { data: sprayData, error: sprayError, isLoading: isLoadingSpray } = useGetSprayByIdQuery(
+    offer.Rewards[0].ItemID
+  );
 
-    const onCardPress = useCallback(() => {
-        if (sprayData) {
-            navigate.navigate("SprayDetails", {
-                spray: sprayData.data,
-                offer,
-            });
-        }
-    }, [navigate, sprayData, offer]);
-
-    const renderSprays = useMemo(() => {
-        if (!sprayData) return null;
-
-        return [sprayData.data.fullIcon, sprayData.data.fullTransparentIcon, sprayData.data.displayIcon].map((iconUrl, index) => (
-            <View key={index} style={styles.sprayImageContainer}>
-                <Image
-                    borderRadius={8}
-                    source={{ uri: iconUrl }}
-                    style={styles.sprayImage}
-                    resizeMode="contain"
-                    height={512}
-                />
-            </View>
-        ));
-    }, [sprayData]);
-
-    if (isLoadingSpray) {
-        return <CardSpraySkeleton />;
+  const onCardPress = useCallback(() => {
+    if (sprayData) {
+      navigate.navigate("SprayDetails", {
+        spray: sprayData.data,
+        offer,
+      });
     }
+  }, [navigate, sprayData, offer]);
 
-    if (sprayError || !sprayData) {
-        return <Error />;
-    }
+  const renderSprays = useMemo(() => {
+    if (!sprayData) return null;
 
-    const spray = sprayData.data;
-
-    return (
-        <TouchableRipple
-            borderless
-            key={offer.OfferID}
-            onPress={onCardPress}
-            rippleColor="rgba(255, 70, 86, .20)"
-            style={[styles.container, { backgroundColor: colors.card }]}
-        >
-            <ImageBackground
-                style={styles.imageBackground}
-                blurRadius={50}
-                source={{ uri: spray.displayIcon }}
-            >
-                <Text variant="titleLarge" style={styles.title} numberOfLines={1}>
-                    {removeCardType(spray.displayName, "spray")}
-                </Text>
-                <View style={styles.sprayContainer}>
-                    {renderSprays}
-                </View>
-                <CostPoint currencyId={Object.keys(offer.Cost)[0]} cost={offer.Cost[Object.keys(offer.Cost)[0]]} />
-            </ImageBackground>
-        </TouchableRipple>
+    return [sprayData.data.fullIcon, sprayData.data.fullTransparentIcon, sprayData.data.displayIcon].map(
+      (iconUrl, index) => (
+        <View key={index} style={styles.sprayImageContainer}>
+          <Image borderRadius={8} source={{ uri: iconUrl }} style={styles.sprayImage} resizeMode="contain" height={512} />
+        </View>
+      )
     );
+  }, [sprayData]);
+
+  if (isLoadingSpray) {
+    return <CardSpraySkeleton />;
+  }
+
+  if (sprayError || !sprayData) {
+    return <Error />;
+  }
+
+  const spray = sprayData.data;
+
+  return (
+    <TouchableRipple
+      borderless
+      key={offer.OfferID}
+      onPress={onCardPress}
+      rippleColor={hexToRgba(palette.primary, 0.2)}
+      style={[styles.container, { backgroundColor: palette.card }]}
+    >
+      <ImageBackground style={styles.imageBackground} blurRadius={50} source={{ uri: spray.displayIcon }}>
+        <Text variant="titleLarge" style={styles.title} numberOfLines={1}>
+          {removeCardType(spray.displayName, "spray")}
+        </Text>
+        <View style={styles.sprayContainer}>{renderSprays}</View>
+        <CostPoint currencyId={Object.keys(offer.Cost)[0]} cost={offer.Cost[Object.keys(offer.Cost)[0]]} />
+      </ImageBackground>
+    </TouchableRipple>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        borderRadius: 16,
-        overflow: "hidden",
-    },
-    imageBackground: {
-        gap: 16,
-        padding: 16,
-    },
-    title: {
-        textTransform: "capitalize",
-    },
-    sprayContainer: {
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    sprayImageContainer: {
-        width: 92,
-        height: 92,
-    },
-    sprayImage: {
-        flex: 1,
-        height: 70,
-    },
+  container: {
+    flex: 1,
+    borderRadius: 16,
+    overflow: "hidden",
+  },
+  imageBackground: {
+    gap: 16,
+    padding: 16,
+  },
+  title: {
+    textTransform: "capitalize",
+  },
+  sprayContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  sprayImageContainer: {
+    width: 92,
+    height: 92,
+  },
+  sprayImage: {
+    flex: 1,
+    height: 70,
+  },
 });
 
 export default React.memo(CardSpray);
